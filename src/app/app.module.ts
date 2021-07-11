bec117b6e6e99564381348001aa0edd217c01538
Cuong <nguyen.cuonguuu@gmail.com>
2021-07-11 10:19:12 +0700

    login 1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -67,12 +67,7 @@ const FORM_MODULES = [JsonSchemaModule];
 
 // #region Http Interceptors
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { DefaultInterceptor } from '@core';
-import { SimpleInterceptor } from '@delon/auth';
-const INTERCEPTOR_PROVIDES = [
-  { provide: HTTP_INTERCEPTORS, useClass: SimpleInterceptor, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true },
-];
+
 // #endregion
 
 // #region Startup Service
@@ -116,7 +111,7 @@ import { STWidgetModule } from './shared/st-widget/st-widget.module';
     ...GLOBAL_THIRD_MODULES,
     ...FORM_MODULES,
   ],
-  providers: [...LANG_PROVIDES, ...INTERCEPTOR_PROVIDES, ...I18NSERVICE_PROVIDES, ...APPINIT_PROVIDES],
+  providers: [...LANG_PROVIDES, ...I18NSERVICE_PROVIDES, ...APPINIT_PROVIDES],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -3,17 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { SimpleGuard } from '@delon/auth';
 // layout
 import { LayoutBasicComponent } from '../layout/basic/basic.component';
+import { AuthGuard } from '../services/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutBasicComponent,
-    canActivate: [SimpleGuard],
-    canActivateChild: [SimpleGuard],
     data: {},
     children: [
       { path: '', redirectTo: 'epd-heading', pathMatch: 'full' },
-      { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule) },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+        canActivate: [AuthGuard],
+      },
       { path: 'delon', loadChildren: () => import('./delon/delon.module').then((m) => m.DelonModule) },
       { path: 'extras', loadChildren: () => import('./extras/extras.module').then((m) => m.ExtrasModule) },
       { path: 'epd-heading', loadChildren: () => import('./epd/epd.module').then((m) => m.EpdModule) },
