59322535cd8b860086221db3ed02d1ec2d219b7b
Cuong <nguyen.cuonguuu@gmail.com>
2021-07-21 21:16:50 +0700

    account setting initial commit

---
+++ src/app/routes/account/pages/account-home-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lca-account-home-page',
+  templateUrl: './account-home-page.component.html',
+  styleUrls: ['./account-home-page.component.less'],
+})
+export class AccountHomePageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
