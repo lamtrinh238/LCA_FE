f310434f4df8e8f65da18573f3c8382b81718052
Thanh Tran Dang <trandangthanh@outlook.com>
2021-07-24 10:47:53 +0700

    go to login

--- src/app/core/domain/services/auth.guard.ts
+++ src/app/core/domain/services/auth.guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard implements CanActivate {
       return true;
     }
 
-    this.router.navigate(['passport/login']);
+    this.authenticationService.gotoLogin();
     return false;
   }
 }
