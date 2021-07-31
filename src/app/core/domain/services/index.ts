36dcf11bffb08c360aa2cfdb5a8673ea66d7df73
Thanh Tran Dang <trandangthanh@outlook.com>
2021-07-31 12:48:50 +0700

    reset password env

--- src/app/core/domain/services/index.ts
+++ src/app/core/domain/services/index.ts
@@ -11,3 +11,5 @@ export * from './client-group.service';
 export * from './program-module.service';
 export * from './epdpcr.service';
 export * from './change-password.service';
+export * from './forgot-password.service';
+export * from './reset-password.service';
