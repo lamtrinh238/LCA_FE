ff7f15283684fc0836184dea224cbc425d2dc8b4
Cuong <nguyen.cuonguuu@gmail.com>
2021-08-06 16:11:40 +0700

    staging 2

--- src/app/routes/user/components/update-user/update-user.component.ts
+++ src/app/routes/user/components/update-user/update-user.component.ts
@@ -1,11 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Validators } from '@angular/forms';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ClientService, QueryParamObject, UserModel, UserService } from '@core';
+import { ClientService, FilterObject, QueryParamObject, UserModel, UserService } from '@core';
 import keys from 'lodash/keys';
 import { NzModalService } from 'ng-zorro-antd/modal';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { debounceTime, switchMap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, debounceTime, filter, finalize, map, switchMap, tap } from 'rxjs/operators';
 
 @Component({
     selector: 'lca-update-user',
@@ -14,15 +15,21 @@ import { debounceTime, switchMap } from 'rxjs/operators';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UpdateUserComponent implements OnInit {
-    private searchChange$ = new BehaviorSubject('');
     queryObject: QueryParamObject;
     formGroup: FormGroup;
     @Input() data: UserModel;
     listOfCurrentPageData: any = [];
-    UserComLinks: UserModel[] = [];
+    UserComLinks: any;
     isLoading = false;
-    optionList: string[] = [];
+
+    selectedUser?: any;
+
+    optionList: any;
+
+    searchChange$ = new BehaviorSubject('');
+
     constructor(
+        private http: HttpClient,
         private _formBuilder: FormBuilder,
         private _nzModalService: NzModalService,
         private _userService: UserService,
@@ -64,33 +71,43 @@ export class UpdateUserComponent implements OnInit {
         });
 
         this.formGroup.patchValue(this.data);
-        this.getCompanyLinks().subscribe((comlink) => {
-            this.isLoading = true;
-            // @ts-ignore
-            this.UserComLinks = comlink.companies;
-            this.isLoading = false;
-        });
-        this.getCompanylist().subscribe((com) => (this.optionList = com));
 
+        this._userService.get(this.data.usrId!).subscribe({
+            next: (comlink) => (this.UserComLinks = comlink.companies),
+        });
+
+        this.loadCompanyFilter();
+    }
+
+    onSearch($event: string): void {
+        this.isLoading = true;
+        this.searchChange$.next($event);
+    }
+
+    loadCompanyFilter(): void {
         const optionList$: Observable<string[]> = this.searchChange$
             .asObservable()
             .pipe(debounceTime(500))
-            .pipe(switchMap(this.getCompanylist));
+            .pipe(
+                tap((res) => {
+                    if (res != '') {
+                        this.queryObject.filter.push(new FilterObject('filterText', res, 'equal'));
+                    }
+                }),
+            )
+            .pipe(
+                switchMap((res) => {
+                    return this.getCompanylist(this.queryObject);
+                }),
+            );
         optionList$.subscribe((data) => {
             this.optionList = data;
+            this.isLoading = false;
         });
     }
 
-    onSearch(value: string): void {
-        this.searchChange$.next(value);
-    }
-
-    getCompanyLinks(): Observable<UserModel> {
-        return this._userService.get(this.data.usrId!);
-    }
-
-    getCompanylist(): Observable<any> {
-        return this._userService.filter(this.queryObject);
+    getCompanylist(filterValue: QueryParamObject): Observable<any> {
+        return this._clientService.filter(filterValue);
     }
 
     submitForm(value: unknown): void {
@@ -103,7 +120,20 @@ export class UpdateUserComponent implements OnInit {
         });
     }
 
-    onCurrentPageDataChange($event: UserModel[]): void {
-        this.listOfCurrentPageData = $event;
+    onClickSave(): void {
+        const headers = {
+            'Content-Type': 'application/json',
+            mode: 'no-cors',
+            'Access-Control-Allow-Origin': 'http://localhost:4200',
+        };
+        const body = {
+            id: null,
+            usrId: this.data.usrId,
+            comId: this.selectedUser[0].comId,
+            usrType: null,
+        };
+        this.http
+            .post('localhost:44302/api/UserCompLink/usercompLink', body, { headers })
+            .subscribe((user) => console.log(user));
     }
 }
