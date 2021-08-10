78c45b3f2200251e27a3a31560d44c42fb8334aa
Thanh Tran Dang <trandangthanh@outlook.com>
2021-08-10 23:51:20 +0700

    update-multilanguage

--- src/app/core/domain/services/company-select.guard.ts
+++ src/app/core/domain/services/company-select.guard.ts
@@ -5,16 +5,20 @@ import { AuthenticationService } from './authentication.service';
 import { SessionService } from './session.service';
 @Injectable({ providedIn: 'root' })
 export class CompanySelectGuard implements CanActivate {
-  constructor(private router: Router, private authenticationService: AuthenticationService, private sessionService: SessionService) {}
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot,
-  ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    if (this.sessionService.authenticatedUserSnapshot?.hasActiveCompany()) {
-      return true;
-    }
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private sessionService: SessionService,
+    ) {}
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot,
+    ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        if (this.sessionService.authenticatedUserSnapshot?.hasActiveCompany()) {
+            return true;
+        }
 
-    this.sessionService.openSelectCompnay();
-    return false;
-  }
+        this.sessionService.openSelectCompnay();
+        return false;
+    }
 }
--- src/app/routes/user/user-routing.module.ts
+++ src/app/routes/user/user-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanySelectGuard } from '@core';
 import { UserChangePasswordComponent } from './pages/user-change-password/user-change-password.component';
 import { UserHomePageComponent } from './pages/user-home-page.component';
 
 const routes: Routes = [
-  { path: '', component: UserHomePageComponent },
-  { path: 'change-password', component: UserChangePasswordComponent },
+    { path: '', component: UserHomePageComponent, canActivate: [CompanySelectGuard] },
+    { path: 'change-password', component: UserChangePasswordComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class UserRoutingModule {}
